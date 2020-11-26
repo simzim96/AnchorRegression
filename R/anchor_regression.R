81be7e23392e1237abd4f5632837eb3e2b5fcba6
Simon Zimmermann <sm.zimmermann@live.de>
2020-11-26 23:47:44 +0100

    deleted self reference

--- R/anchor_regression.R
+++ R/anchor_regression.R
@@ -14,6 +14,22 @@
 #' @export
 #' @importFrom glmnet glmnet cv.glmnet
 #' @importFrom stats coef lm
+#' @examples
+#' x <- as.data.frame(matrix(data = rnorm(100),nrow = 10,ncol = 10))
+#' anchor <- as.data.frame(matrix(data = rnorm(20),nrow = 10,ncol = 2))
+#' colnames(anchor) <- c('X1','X2')
+#' gamma <- 2
+#' target_variable <- 'V2'
+#'
+#' anchor_regression(x, anchor, gamma, target_variable)
+#'
+
+
+
+
+
+
+
 
 
 anchor_regression <- function(x, anchor, gamma, target_variable){
