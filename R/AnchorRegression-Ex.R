be967c0b5821969b0831a4a335d8c124630c90a0
Simon Zimmermann <sm.zimmermann@live.de>
2020-11-26 23:41:09 +0100

    added example

---
+++ R/AnchorRegression-Ex.R
@@ -0,0 +1,13 @@
+library(AnchorRegression)
+
+
+x <- as.data.frame(matrix(data = rnorm(100),nrow = 100,ncol = 10))
+
+anchor <- as.data.frame(matrix(data = rnorm(20),nrow = 100,ncol = 2))
+colnames(anchor) <- c('X1','X2')
+gamma <- 2
+
+target_variable <- 'V2'
+
+
+anchor_regression(x, anchor, gamma, target_variable)
